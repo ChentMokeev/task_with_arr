6d6d27ce89fb97ab8531c4a65459b91bbed0a252
Chent Mokeev <chint2184@gmail.com>
2021-12-20 12:31:40 +0600

    refactored using functions

--- task1.dart
+++ task1.dart
@@ -1,9 +1,13 @@
-void main(List<String> args) {
+void main() {
   List l1 = [1, 2, 4];
   List l2 = [1, 3, 4];
 
-  List res = l1 + l2;
+  print(combineAndSort(l1, l2));
+}
+
+List combineAndSort(List arr1, List arr2) {
+  List res = arr1 + arr2;
 
   res.sort();
-  print(res);
-} 
+  return res;
+}
--- task2.dart
+++ task2.dart
@@ -3,32 +3,35 @@ import 'dart:io';
 void main() {
   var s = stdin.readLineSync()!;
   List chars = s.split('');
+
+  print(checkBracketClosed(chars));
+}
+
+bool checkBracketClosed(List anyList) {
   var openIndex, closeIndex;
   List stack = [];
   List open = ['{', '(', '['];
   List close = ['}', ')', ']'];
 
-  for (int i = 0; i < chars.length; i++) {
-    openIndex = open.indexOf(chars[i]);
+  for (int i = 0; i < anyList.length; i++) {
+    openIndex = open.indexOf(anyList[i]);
     if (openIndex != -1) {
       stack.add(openIndex);
       continue;
     }
 
-    closeIndex = close.indexOf(chars[i]);
+    closeIndex = close.indexOf(anyList[i]);
     if (closeIndex != -1) {
       openIndex = stack.removeLast();
       if (closeIndex != openIndex) {
-        print(false);
-        return;
+        return false;
       }
     }
   }
 
   if (stack.length != 0) {
-    print(false);
-    return;
+    return false;
   }
 
-  print(true);
+  return true;
 }
